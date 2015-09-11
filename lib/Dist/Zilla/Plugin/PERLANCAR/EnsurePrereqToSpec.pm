0919f8aedb36c0a3423d1eb8d6de79a3cb3f54f8
perlancar (@pc-office) <perlancar@gmail.com>
2015-09-11 16:41:11 +0700

    Initial commit

---
+++ lib/Dist/Zilla/Plugin/PERLANCAR/EnsurePrereqToSpec.pm
@@ -0,0 +1,87 @@
+package Dist::Zilla::Plugin::PERLANCAR::EnsurePrereqToSpec;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+use Moose;
+use namespace::autoclean;
+
+with (
+    'Dist::Zilla::Role::MetaProvider',
+    'Dist::Zilla::Role::Rinci::CheckDefinesMeta',
+);
+
+sub _prereq_check {
+    my ($self, $prereqs_hash, $mod, $wanted_phase, $wanted_rel) = @_;
+
+    my $num_any = 0;
+    my $num_wanted = 0;
+    for my $phase (keys %$prereqs_hash) {
+        for my $rel (keys %{ $prereqs_hash->{$phase} }) {
+            if (exists $prereqs_hash->{$phase}{$rel}{$mod}) {
+                $num_any++;
+                $num_wanted++ if $phase eq $wanted_phase && $rel eq $wanted_rel;
+            }
+        }
+    }
+    ($num_any, $num_wanted);
+}
+
+sub _prereq_only_in {
+    my ($self, $prereqs_hash, $mod, $wanted_phase, $wanted_rel) = @_;
+
+    my ($num_any, $num_wanted) = $self->_prereq_check(
+        $prereqs_hash, $mod, $wanted_phase, $wanted_rel,
+    );
+    $num_wanted == 1 && $num_any == 1;
+}
+
+sub _prereq_none {
+    my ($self, $prereqs_hash, $mod) = @_;
+
+    my ($num_any, $num_wanted) = $self->_prereq_check(
+        $prereqs_hash, $mod, 'whatever', 'whatever',
+    );
+    $num_any == 0;
+}
+
+# actually we use MetaProvider just to be after all the PrereqSources plugins
+sub metadata {
+    my $self = shift;
+
+    my $prereqs_hash = $self->zilla->prereqs->as_string_hash;
+
+    # Rinci
+    if ($self->check_dist_defines_rinci_meta) {
+        $self->log_fatal(["Dist defines Rinci metadata, but there is no DevelopRecommends prereq to Rinci"])
+            unless $self->_prereq_only_in($prereqs_hash, "Rinci", "develop", "recommends");
+    } else {
+        $self->log_fatal(["Dist does not define Rinci metadata, but there is a prereq to Rinci"])
+            unless $self->_prereq_none($prereqs_hash, "Rinci");
+    }
+}
+
+__PACKAGE__->meta->make_immutable;
+1;
+# ABSTRACT: Ensure prereq to spec modules
+
+=for Pod::Coverage .+
+
+=head1 SYNOPSIS
+
+In C<dist.ini>:
+
+ [PERLANCAR::EnsurePrereqToSpec]
+
+
+=head1 DESCRIPTION
+
+I like to specify prerequisite to spec modules such as L<Rinci>, L<Riap>,
+L<Sah>, L<Setup>, etc as DevelopRecommends, to express that a distribution
+conforms to such specification(s).
+
+Currently only L<Rinci> is checked.
